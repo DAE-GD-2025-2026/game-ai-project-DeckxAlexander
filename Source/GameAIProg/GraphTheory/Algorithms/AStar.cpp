585a3a21c248d4658d817dcab8039d2af9df7594
Alexander Deckx <alexander.deckx@student.howest.be>
2026-03-11 12:02:30 +0100

    Added BFS and A*

---
+++ Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
@@ -0,0 +1,109 @@
+﻿#include "AStar.h"
+
+using namespace GameAI;
+
+AStar::AStar(Graph* const pGraph, HeuristicFunctions::Heuristic hFunction)
+	: pGraph(pGraph)
+	, HeuristicFunction(hFunction)
+{
+}
+
+std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
+{
+	std::vector<Node*> path{};
+	
+	std::vector<NodeRecord> openList;
+	std::vector<NodeRecord> closedList;
+	NodeRecord currentNodeRecord{};
+	NodeRecord startRecord{pStartNode, nullptr, 
+		0.f,GetHeuristicCost(pStartNode, pGoalNode)};
+	
+	auto nodes = pGraph->GetActiveNodes();
+	
+	openList.push_back(startRecord);
+	
+	while (!openList.empty())
+	{
+		auto RecordIt = std::min_element(openList.begin(), openList.end());
+		currentNodeRecord = *RecordIt;
+		if (currentNodeRecord.pNode == pGoalNode)
+		{
+			while (currentNodeRecord.pNode != pStartNode)
+			{
+				path.push_back(currentNodeRecord.pNode);
+				if (currentNodeRecord.pConnection == nullptr)
+					break;
+				Node* pFromNode = nodes[currentNodeRecord.pConnection->GetFromId()];
+
+				auto it = std::find_if(
+					closedList.begin(),
+					closedList.end(),
+					[pFromNode](const NodeRecord& record)
+					{
+						return record.pNode == pFromNode;
+					});
+
+				if (it == closedList.end())
+					break;
+
+				currentNodeRecord = *it;
+			}
+			
+			path.push_back(pStartNode);
+			std::reverse(path.begin(), path.end());
+			
+			return path;
+		}
+		
+		
+		for (Connection* connection : pGraph->FindConnectionsFrom(currentNodeRecord.pNode->GetId()))
+		{
+			Node* pNextNode = nodes[connection->GetToId()];
+			float gCost = currentNodeRecord.costSoFar + connection->GetWeight();
+			auto closedListNode = std::find_if(closedList.begin(),closedList.end(),[pNextNode](const NodeRecord& record){return record.pNode == pNextNode;});
+			if (closedListNode != closedList.end())
+			{
+				if (closedListNode->costSoFar <= gCost)
+				{
+					
+					continue;
+				}
+				closedList.erase(closedListNode);
+			}
+			
+			auto openListNode = std::find_if(openList.begin(),openList.end(),[pNextNode](const NodeRecord& record){return record.pNode == pNextNode;});
+			if (openListNode != openList.end())
+			{
+				if (openListNode->costSoFar <= gCost)
+				{
+					continue;
+				}
+
+				openList.erase(openListNode);
+			}
+			NodeRecord newRecord{pNextNode, connection, gCost,gCost+GetHeuristicCost(pNextNode, pGoalNode)};
+			openList.push_back(newRecord);
+			
+			
+		}
+		closedList.push_back(currentNodeRecord);
+			
+		
+		auto it = std::find(openList.begin(), openList.end(),
+			currentNodeRecord);
+
+		if (it != openList.end())
+		{
+			openList.erase(it);
+		}
+		
+	}
+	
+	return path;
+}
+
+float AStar::GetHeuristicCost(Node* const pStartNode, Node* const pEndNode) const
+{
+	FVector2D toDestination = pGraph->GetNode(pEndNode->GetId())->GetPosition() - pGraph->GetNode(pStartNode->GetId())->GetPosition();
+	return HeuristicFunction(abs(toDestination.X), abs(toDestination.Y));
+}
---
+++ Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
@@ -0,0 +1,66 @@
+﻿#include "BFS.h"
+
+#include <map>
+#include <queue>
+
+#include "Shared/Graph/Graph.h"
+
+using namespace GameAI;
+
+BFS::BFS(Graph* const pGraph)
+	: pGraph(pGraph)
+{
+}
+
+// TODO Breath First Search Algorithm searches for a path from the startNode to the destinationNode
+std::vector<Node*> BFS::FindPath(Node* const pStartNode, Node* const pDestinationNode) const
+{
+	std::vector<Node*> Path;
+	
+	TQueue<Node*> OpenList;
+	TSet<Node*> ClosedList;
+	TMap<Node*, Node*> Parent;
+
+	// Start node
+	OpenList.Enqueue(pStartNode);
+	ClosedList.Add(pStartNode);
+
+	Node* CurrentNode = nullptr;
+
+	while (OpenList.Dequeue(CurrentNode))
+	{
+		if (CurrentNode == pDestinationNode)
+		{
+			// Backtracking
+			Node* Step = pDestinationNode;
+
+			while (Step != nullptr)
+			{
+				Path.push_back(Step);
+				
+				if (!Parent.Contains(Step)) break;
+
+				Step = Parent[Step];
+			}
+
+			std::reverse(Path.begin(), Path.end());
+			return Path;
+		}
+
+		// Check neighbors
+		for (Connection* NeighborConnection : pGraph->FindConnectionsFrom(CurrentNode->GetId()))
+		{
+			Node* Neighbor = pGraph->GetActiveNodes()[NeighborConnection->GetToId()];
+			if (!ClosedList.Contains(Neighbor))
+			{
+				OpenList.Enqueue(Neighbor);
+				ClosedList.Add(Neighbor);
+
+				// Store previous node
+				Parent.Add(Neighbor, CurrentNode);
+			}
+		}
+	}
+
+	return Path;
+}
